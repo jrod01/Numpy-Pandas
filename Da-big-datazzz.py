a7b3d79f6acfc36a0d4fb0d47dd44e121ce3b861
Jonatan Rodriguez <jrguezg1@gmail.com>
2013-07-15 17:20:03 -0400

    More data frame
    Rows in the data frame

--- Da-big-datazzz.py
+++ Da-big-datazzz.py
@@ -1076,6 +1076,120 @@ Ryan     3
 
 #DataFrame
 
+In [2]: from pandas import Series, DataFrame
+
+In [3]: import pandas as pd
+
+In [7]: data = {'state': ['Ohio', 'Ohio', 'Ohio', 'Nevada', 'Nevada'], 'year': [2000, 2001, 2002, 2001, 2002],
+   ...: 'pop': [1.5, 1.7, 3.6, 2.4, 2.9]}
+
+In [8]: frame = DataFrame(data)
+
+In [9]: frame
+Out[9]:
+   pop   state  year
+0  1.5    Ohio  2000
+1  1.7    Ohio  2001
+2  3.6    Ohio  2002
+3  2.4  Nevada  2001
+4  2.9  Nevada  2002
+
+In [10]: DataFrame(data,columns=['year', 'state', 'pop'])
+Out[10]:
+   year   state  pop
+0  2000    Ohio  1.5
+1  2001    Ohio  1.7
+2  2002    Ohio  3.6
+3  2001  Nevada  2.4
+4  2002  Nevada  2.9
+
+
+In [13]: frame2 = DataFrame(data, columns=['year', 'state', 'pop', 'debt'],
+   ....: index=['one', 'two', 'three', 'four', 'five'])
+
+In [14]: frame2
+Out[14]:
+       year   state  pop debt
+one    2000    Ohio  1.5  NaN
+two    2001    Ohio  1.7  NaN
+three  2002    Ohio  3.6  NaN
+four   2001  Nevada  2.4  NaN
+five   2002  Nevada  2.9  NaN
+
+In [15]: frame2
+Out[15]:
+       year   state  pop debt
+one    2000    Ohio  1.5  NaN
+two    2001    Ohio  1.7  NaN
+three  2002    Ohio  3.6  NaN
+four   2001  Nevada  2.4  NaN
+five   2002  Nevada  2.9  NaN
+
+In [16]: frame2.columns
+Out[16]: Index([year, state, pop, debt], dtype=object)
+
+
+In [18]: frame2['state']
+Out[18]:
+one        Ohio
+two        Ohio
+three      Ohio
+four     Nevada
+five     Nevada
+Name: state
+
+In [19]: frame2.year
+Out[19]:
+one      2000
+two      2001
+three    2002
+four     2001
+five     2002
+Name: year
+
+In [20]: frame2.ix['three']
+Out[20]:
+year     2002
+state    Ohio
+pop       3.6
+debt      NaN
+Name: three
+
+In [21]: frame2['debt'] = 16.5
+
+In [22]: frame2
+Out[22]:
+       year   state  pop  debt
+one    2000    Ohio  1.5  16.5
+two    2001    Ohio  1.7  16.5
+three  2002    Ohio  3.6  16.5
+four   2001  Nevada  2.4  16.5
+five   2002  Nevada  2.9  16.5
+
+In [23]: frame2['debt'] = np.arange(5.)
+
+In [24]: frame2
+Out[24]:
+       year   state  pop  debt
+one    2000    Ohio  1.5     0
+two    2001    Ohio  1.7     1
+three  2002    Ohio  3.6     2
+four   2001  Nevada  2.4     3
+five   2002  Nevada  2.9     4
+
+In [25]: val = Series([-1.2, -1.5, -1.7], index=['two', 'four', 'five'])
+
+In [26]: frame2['debt'] = val
+
+In [27]: frame2
+Out[27]:
+       year   state  pop  debt
+one    2000    Ohio  1.5   NaN
+two    2001    Ohio  1.7  -1.2
+three  2002    Ohio  3.6   NaN
+four   2001  Nevada  2.4  -1.5
+five   2002  Nevada  2.9  -1.7
+
 
 
 
